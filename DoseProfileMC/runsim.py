53963126fc4afbb4ac11eae67e6e445bbd37dc89
simonge <simongardner1@hotmail.com>
2022-10-03 20:44:36 +0100

    runsim now creates and deletes macro file and runs the simulation creating a suitably named output file

--- DoseProfileMC/runsim.py
+++ DoseProfileMC/runsim.py
@@ -4,12 +4,83 @@ import os
 import subprocess
 import sys
 
+macroName = "runmacro.mac"
+
 #Allowed types of particle
 particleTypes = ["gamma","electron","proton","ion"]
 
-if(len(sys.argv)==3):
+#Physics flags
+physicsList  = ['standard_opt3','QGSP_BIC_EMY']
+physicsFlag  = 0
+
+#Detector flags
+tumourFlag   = "0"
+
+#nEvents
+nEvents = "10000"
+
+#Check python arguments
+if(len(sys.argv)>=3):
     if any(particle == sys.argv[1] for particle in particleTypes):
-        float(sys.argv[2])
+        partName = sys.argv[1]
+        energy   = sys.argv[2]
+
+        #---------------------
+        # Write Macro file
+        #---------------------
+        outName = "DoseMC_"+partName+"_"+energy+"MeV_"
+
+        if(len(sys.argv)>=4):
+            nEvents = sys.argv[3]
+
+        outName = outName+nEvents+"n_"
+        
+        if(len(sys.argv)>=5):
+            tumourFlag = sys.argv[4]
+            outName = outName + "Tumour_"
+            
+        if(len(sys.argv)>=6):
+            physicsFlag = int(sys.argv[5])
+            outName = outName + "Hadronic_"
+
+        #Construct output file name
+        outName = outName+"Out.csv\n"
+        
+        #Write macro file
+        f = open(macroName, "w")
+
+        f.write("/DoseProfileMC/analysis/setOutputFile " + outName)
+        
+        f.write("/DoseProfileMC/detector/TumourOn "+tumourFlag+"\n")
+        f.write("/DoseProfileMC/detector/TumourRadius 1.0\n")
+        f.write("/DoseProfileMC/detector/TumourHeight 1.0\n")
+        
+        f.write("/DoseProfileMC/physics/addPhysics "+physicsList[physicsFlag]+"\n")
+        f.write("/run/initialize\n")
+
+        f.write("/gps/particle "+partName+"\n")
+        f.write("/gps/energy "+sys.argv[2]+" MeV\n")
+        f.write("/gps/direction -1 0 0\n")
+        f.write("/gps/pos/type Plane\n")
+        f.write("/gps/pos/shape Circle\n")
+        f.write("/gps/pos/radius 1 mm\n")
+        f.write("/gps/pos/centre 0 100 0 mm\n")
+        f.write("/gps/pos/rot1 1 0 0\n")
+        f.write("/gps/pos/rot2 0 0 1\n")
+        f.write("/run/beamOn "+nEvents+"\n")
+        f.close()
+
+
+        #--------------------------
+        # Run simulation
+        #--------------------------
+        subprocess.run(["./DoseProfileMC",macroName])
+
+
+        os.remove(macroName)
+
+
+
         
     else:
         print('WARNING, wrong input: please re-run as eg ./runsim.py [beam type] [energy]')
