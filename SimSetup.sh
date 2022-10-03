2e51b5f8a54a6aaa5540ec015a06570df19b081f
simonge <simongardner1@hotmail.com>
2022-10-03 22:18:44 +0100

    Simplified SimSetup

--- SimSetup.sh
+++ SimSetup.sh
@@ -2,12 +2,6 @@
 
 mkdir -p ~/data
 
-mv bashrc ~/.bashrc
-source ~/.bashrc
+cp /local/cern/software/Sim/bashrc ~/.bashrc
 
-mv ~/Sim/PHYS5036_setup/SimTools2021/SodiumIodideMC ~/
-mv ~/Sim/PHYS5036_setup/SimTools2021/DoseProfileMC ~/
-
-cd ~/DoseProfileMC/ ; gmake clean ; gmake ; cd ~
-cd ~/SodiumIodideMC/ ; gmake clean ; gmake ; cd ~
 bash
