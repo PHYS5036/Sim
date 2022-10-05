a2b58ad2cbdb8eb29470d997cadf5b33d4fc7ed1
simonge <simongardner1@hotmail.com>
2022-10-05 20:39:17 +0100

    Added data directory creation and notebook copy to setup script

--- SimSetup.sh
+++ SimSetup.sh
@@ -5,4 +5,8 @@ mkdir -p ~/data
 cp -n ~/.bashrc ~/bashrc.orig
 cp /local/cern/software/Sim/bashrc ~/.bashrc
 
+mkdir ~/data
+cd ~/data
+cp -n /local/cern/software/Notebooks/DoseProfileAnalysis.ipynb ./
+
 bash
