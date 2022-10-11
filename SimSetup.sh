b8fa55b2f653759fcc8777217bb8aa7e8e7bdad5
simonge <simongardner1@hotmail.com>
2022-10-11 11:38:09 +0100

    Copy folder rather than universalfit

--- SimSetup.sh
+++ SimSetup.sh
@@ -6,6 +6,6 @@ cp /local/cern/software/Sim/bashrc ~/.bashrc
 mkdir -p ~/data
 cd ~/data
 cp -n /local/cern/software/Notebooks/DoseProfileAnalysis.ipynb ./
-cp -n /local/cern/software/Notebooks/UniversalFit.ipynb ./
+cp -rn /local/cern/software/PHYS5036_Analysis ./
 
 bash
