09e964f7e1db2e8c97d1bed83daf4ef0a8fd6322
William-OD <william.od.wod@gmail.com>
2025-10-08 15:51:46 +0100

    Ensured output dirs are created/exist and outputs for both experiments are set correctly.

--- bin/runDoseProfile.py
+++ bin/runDoseProfile.py
@@ -6,15 +6,28 @@ import sys
 
 macroName = "runmacro.mac"
 
-#Allowed types of particle
+# Determine paths are set.
+project_dir = os.environ.get('PROJECT_DIR')
+if not project_dir:
+    print("Error: PROJECT_DIR environment variable is not set. Make sure to source bash script.")
+    sys.exit(1)
+
+output_dir = os.environ.get('OUTPUT_DIR')
+if not output_dir:
+    print("Error: OUTPUT_DIR environment variable is not set. Make sure to source bash script.")
+    sys.exit(1)
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Allowed types of particle
 particleTypes = ["gamma","electron","proton","ion"]
 particleDict  = {"gamma":"gamma", "electron":"e-", "proton":"proton", "ion":"ion"}
 
-#Physics flags
+# Physics flags
 physicsList  = ['standard_opt3','QGSP_BIC_EMY']
 physicsFlag  = 0
 
-#Detector flags
+# Detector flags
 tumourFlag   = "0"
 
 #nEvents
@@ -29,7 +42,7 @@ if(len(sys.argv)>=3):
         #---------------------
         # Write Macro file
         #---------------------
-        outName = "DoseMC_"+partName+"_"+energy+"MeV_"
+        outName = outdir + "DoseMC_"+partName+"_"+energy+"MeV_"
 
         if(len(sys.argv)>=4):
             nEvents = sys.argv[3]
@@ -83,11 +96,7 @@ if(len(sys.argv)>=3):
         #--------------------------
         subprocess.run(["DoseProfileMC",macroName])
 
-
         os.remove(macroName)
-
-
-
         
     else:
         print('WARNING, wrong input: please re-run as eg ./runsim.py [beam type] [energy] [N events] [Tumour 0/1] [Hadronic 0/1]')
